dcbd3dad6c1cd9f86452e370af603b512c739ed7
Jeeyoung Kim <jee@squareup.com>
2015-06-17 11:39:41 -0700

    initial commit

--- aggregator.go
+++ aggregator.go
@@ -1,3 +1,27 @@
 package aggregator
 
+// simple test of aggregating system.
 
+type Message struct {
+	timestamp int64
+	key       string
+	value     int
+}
+
+// Partitioner accepts the message and publishes them to the corresponding aggregator.
+type Partitioner interface {
+	Accept(Message)
+}
+
+// Aggregator accepts the message.
+type Aggregator interface {
+	Accept(Message)
+}
+
+type Scheduler interface {
+	RegisterAggregator(string, Aggregator)
+	GetAggregator(int64, string) Aggregator
+}
+
+type Environment struct {
+}
